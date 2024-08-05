010af388dd0a285e57b61be22771b3548358c1a9
Chibuzor-Egbo <chibuzoregbo04@gmail.com>
2024-08-05 10:39:34 +0100

    Add cart page

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -5,11 +5,15 @@ import card_cover_2 from "./card-cover-2.png";
 import card_cover_3 from "./card-cover-3.png";
 import card_cover_4 from "./card-cover-4.png";
 import hooli from "./hooli.png";
+import mastercard from "./mastercard.png";
+import paystack from "./paystack.png";
 import piper_hat from "./piper_hat.png";
 import redit from "./reddit.png";
 import stripe from "./stripe.png";
 import user from "./user.png";
+import visa from "./visa.png";
 import yft from "./yft.png";
+
 export {
   aws,
   bg_1,
@@ -18,9 +22,12 @@ export {
   card_cover_3,
   card_cover_4,
   hooli,
+  mastercard,
+  paystack,
   piper_hat,
   redit,
   stripe,
   user,
+  visa,
   yft,
 };
